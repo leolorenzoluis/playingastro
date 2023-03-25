6b575e8b451787cf82bd0a3b52ce3933ca7cca9f
leolorenzoluis <leolorenzoluis@outlook.com>
2023-03-24 22:14:25 -0400

    added subscribe

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 // https://astro.build/config
 import cloudflare from '@astrojs/cloudflare';
 
@@ -11,12 +12,16 @@ export default defineConfig({
   integrations: [preact(), tailwind(), mdx()],
   output: 'server',
   adapter: cloudflare(),
-  define: {
-    'process.env.AIRTABLE_API_KEY': JSON.stringify(
-      process.env.AIRTABLE_API_KEY
-    ),
-    'process.env.AIRTABLE_BASE_ID': JSON.stringify(
-      process.env.SPOTIFY_CLIENAIRTABLE_BASE_IDT_SECRET
-    ),
+
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+    define: {
+      'process.env.AIRTABLE_API_KEY': JSON.stringify(
+        process.env.AIRTABLE_API_KEY
+      ),
+      'process.env.AIRTABLE_BASE_ID': JSON.stringify(
+        process.env.SPOTIFY_CLIENAIRTABLE_BASE_IDT_SECRET
+      ),
+    },
   },
 });
